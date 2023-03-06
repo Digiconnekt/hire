f772d8666ba0aca8212c33c781d290eb41f50015
Rohan Daddekar <rohandaddekar9@gmail.com>
2023-03-06 17:33:27 +0530

    sticky header updated and header links added to scroll to section

--- js/index.js
+++ js/index.js
@@ -1,13 +1,18 @@
 // sticky header start
 window.onload = function () {
   var navbar = document.querySelector(".links-plus-header");
+  var prevScrollPos = window.pageYOffset;
   var sticky = 200;
+
   window.addEventListener("scroll", function () {
-    if (window.pageYOffset >= sticky) {
+    var currentScrollPos = window.pageYOffset;
+    if (currentScrollPos >= sticky && currentScrollPos < prevScrollPos) {
       navbar.classList.add("sticky");
     } else {
       navbar.classList.remove("sticky");
     }
+    prevScrollPos = currentScrollPos;
   });
 };
+
 // sticky header end
